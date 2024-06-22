889b9b23e28d75e8fc184487377a6ca5946c92b4
samcab28 <samircabrera2528@gmail.com>
2024-06-22 15:05:23 -0600

    update en sql

--- src/controllers/ctrCategoriaEjercicio/ConsultCatEje.js
+++ src/controllers/ctrCategoriaEjercicio/ConsultCatEje.js
@@ -1,21 +1,13 @@
-import {MySqlConnection} from "../../database/DBConnection.js";
+import { MySqlConnection } from '../../database/DBConnection.js';
 
-export const  consultCategoriaEjercicio = async (req, res) => {
-    const ejercicioId = req.params.id || 0;
-    let connection;
-
+// Controlador para consultar un cobro
+export const consultCategoriaEjercicio = async (req, res) => {
+    const { id } = req.params;
+    const sql = 'CALL CategoriaEjercicioConsult(?)';
     try {
-        connection = await MySqlConnection.getConnection();
-        const query = ejercicioId ? 'CALL CategoriaEjercicioConsult(?)' : 'CALL CategoriaEjercicioConsult(NULL)';
-        const params = ejercicioId ? [ejercicioId] : [];
-        const [rows] = await connection.execute(query, params);
-        res.json(rows);
-    } catch (error) {
-        console.error('Error al consultar el ejercicio:', error);
-        res.status(500).send('Error en el servidor');
-    } finally {
-        if (connection) connection.release();
+        const [result] = await MySqlConnection.execute(sql, [id || 0]);
+        res.send(result[0]);
+    } catch (err) {
+        res.status(500).send(err.message);
     }
-}
-
-
+};
