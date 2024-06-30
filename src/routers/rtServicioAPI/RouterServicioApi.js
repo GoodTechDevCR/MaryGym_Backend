72e3b671ff08a76f531e922c236514e330d32c2b
samcab28 <samircabrera2528@gmail.com>
2024-06-29 21:54:11 -0600

    pdf completo ok

--- src/routers/rtServicioAPI/RouterServicioApi.js
+++ src/routers/rtServicioAPI/RouterServicioApi.js
@@ -1,21 +1,28 @@
 import { buildPDF } from "../../services/BuildPdf.js";
-
 import express from 'express';
 const router = express.Router();
 
-// Router para la creación de un PDF
+// Ruta para generar el PDF
 router.post("/generarPDF", (req, res) => {
     const jsonData = req.body;
+    const { usuario } = jsonData;  // Obtén el valor de usuario del JSON
+    console.log("usuario usuario: ", usuario);
 
-    const stream = res.writeHead(200, {
+    // Asegúrate de que `usuario` tenga un valor válido
+    if (!usuario) {
+        return res.status(400).json({ error: "El campo usuario es requerido." });
+    }
+
+    // Crea una instancia de stream para la respuesta
+    res.writeHead(200, {
         "Content-Type": "application/pdf",
-        "Content-Disposition": "attachment; filename=prueba.pdf",
+        "Content-Disposition": `attachment; filename=rutina_${usuario}.pdf`,  
     });
 
     buildPDF(
         jsonData,
-        (data) => stream.write(data),
-        () => stream.end()
+        (data) => res.write(data),
+        () => res.end()
     );
 });
 
