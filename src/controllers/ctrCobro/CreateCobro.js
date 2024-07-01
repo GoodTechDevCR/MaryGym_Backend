392edf0a541c78d701da429200580d995cd2a724
Luis <luisgerardourbsalz@gmail.com>
2024-07-01 15:43:27 -0600

    Update CreateCobro.js

--- src/controllers/ctrCobro/CreateCobro.js
+++ src/controllers/ctrCobro/CreateCobro.js
@@ -2,8 +2,8 @@ import { MySqlConnection } from '../../database/DBConnection.js';
 
 // Controlador para crear o actualizar un cobro y registrar un pago
 export const createCobroYPago = async (req, res) => {
-    const { IdUsuario, Monto, FechaPago, IdTipoTran } = req.body;
-    const sql = 'CALL CrearOActualizarCobro(?, ?, ?, ?)';
+    const { IdUsuario, Monto, FechaPago, IdTipoTran, DiasAdicionales, FechaFinalEspecial } = req.body;
+    const sql = 'CALL CrearOActualizarCobro(?, ?, ?, ?, ?, ?)';
 
     // Outputs en la consola para ver los datos recibidos
     console.log('Datos recibidos en el JSON:');
@@ -11,12 +11,15 @@ export const createCobroYPago = async (req, res) => {
     console.log('Monto:', Monto);
     console.log('FechaPago:', FechaPago);
     console.log('IdTipoTran:', IdTipoTran);
+    console.log('DiasAdicionales:', DiasAdicionales);
+    console.log('FechaFinalEspecial:', FechaFinalEspecial); 
     
     try {
-        const [result] = await MySqlConnection.execute(sql, [IdUsuario, Monto, FechaPago, IdTipoTran]);
+        const [result] = await MySqlConnection.execute(sql, [IdUsuario, Monto, FechaPago, IdTipoTran, DiasAdicionales, FechaFinalEspecial]);
         res.send('Cobro y pago creados/actualizados exitosamente');
     } catch (err) {
-        console.error('Error en createCobroYPago:', err);
         res.status(500).send(err.message);
     }
 };
+
+
