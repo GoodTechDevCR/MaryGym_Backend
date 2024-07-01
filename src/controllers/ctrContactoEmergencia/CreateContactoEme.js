70ba109efec0c69ec7bdcc24317439d63655f177
samcab28 <samircabrera2528@gmail.com>
2024-07-01 16:31:24 -0600

    crear contacto emergencia

--- src/controllers/ctrContactoEmergencia/CreateContactoEme.js
+++ src/controllers/ctrContactoEmergencia/CreateContactoEme.js
@@ -1,13 +1,31 @@
 import { MySqlConnection } from '../../database/DBConnection.js';
 
-// Controlador para crear un cobro
+// Controlador para crear contactos de emergencia
 export const createContactoEme = async (req, res) => {
-    const { IdUsuario, Nombre, NumeroTelefono, Relacion } = req.body;
+    const contactos = req.body;  // Esperamos un array de contactos
+
+    // El SQL de llamada al procedimiento almacenado
     const sql = 'CALL ContactoEmergenciaCreate(?, ?, ?, ?)';
+
     try {
-        const [result] = await MySqlConnection.execute(sql, [IdUsuario, Nombre, NumeroTelefono, Relacion]);
-        res.send('Cobro creado');
+        // Crear una conexión de transacción
+        const connection = await MySqlConnection.getConnection();
+        await connection.beginTransaction();
+
+        for (const contacto of contactos) {
+            const { IdUsuario, Nombre, NumeroTelefono, Relacion } = contacto;
+            await connection.execute(sql, [IdUsuario, Nombre, NumeroTelefono, Relacion]);
+        }
+
+        await connection.commit();
+        connection.release();  // Liberar la conexión después de la transacción
+
+        res.send('Contactos de emergencia creados');
     } catch (err) {
+        if (connection) {
+            await connection.rollback();
+            connection.release();
+        }
         res.status(500).send(err.message);
     }
 };
