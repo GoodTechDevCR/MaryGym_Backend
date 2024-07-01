677e4ee315d4b1f5c017986b96cd355244e35263
samcab28 <samircabrera2528@gmail.com>
2024-06-30 21:30:23 -0600

    update en sp de consulta ejercicio

--- src/controllers/ctrEjercicio/ConsultEjercicio.js
+++ src/controllers/ctrEjercicio/ConsultEjercicio.js
@@ -1,15 +1,16 @@
 import {MySqlConnection} from "../../database/DBConnection.js";
 
-export const  consultEjercicio = async (req, res) => {
-    const ejercicioId = req.params.id || 0;
+export const consultEjercicio = async (req, res) => {
+    const ejercicioId = parseInt(req.params.id) || 0; // Asegúrate de que sea un número entero
     let connection;
 
     try {
         connection = await MySqlConnection.getConnection();
-        const query = ejercicioId ? 'CALL EjercicioConsult(?)' : 'CALL EjercicioConsult(NULL)';
-        const params = ejercicioId ? [ejercicioId] : [];
-        const [rows] = await connection.execute(query, params);
-        res.json(rows[0]);
+        const query = 'CALL EjercicioConsult(?)';
+        const [rows] = await connection.execute(query, [ejercicioId]);
+
+        // En MySQL, CALL devuelve el resultado en la primera posición del array de resultados
+        res.json(rows[0]); 
     } catch (error) {
         console.error('Error al consultar el ejercicio:', error);
         res.status(500).send('Error en el servidor');
@@ -17,6 +18,7 @@ export const  consultEjercicio = async (req, res) => {
         if (connection) connection.release();
     }
 }
+
 
 
 export const  consultEjercicioByCat = async (req, res) => {
