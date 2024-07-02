93e9f7fcb40fda4e7ae866813c488f654eb8cfa2
Pamreth29 <pamreth29@gmail.com>
2024-07-02 17:32:40 -0600

    update create user

--- src/controllers/ctrUsuario/CreateUsuario.js
+++ src/controllers/ctrUsuario/CreateUsuario.js
@@ -1,15 +1,41 @@
 import { MySqlConnection } from '../../database/DBConnection.js';
-import {EnvioCorreo} from "../../services/EmailSender.js"
 
 // Controlador para Crear usuario
 export const createUsuario = async (req, res) => {
     const { Nombre, Apellido, Password, Telefono, Correo, Saldo, Estado, FechaNacimiento } = req.body;
-    const sql = 'CALL UsuarioCreate(?, ?, ?, ?, ?, ?, ?, ?)';
+
+    // Verificar que todos los campos requeridos estén presentes y no estén vacíos
+    if (!Nombre || !Apellido || !Password || !Telefono || !Correo || !Saldo || !Estado || !FechaNacimiento) {
+        return res.status(400).json({ alert: 'error', message: 'Todos los campos son requeridos' });
+    }
+
     try {
-        const [result] = await MySqlConnection.execute(sql, [Nombre, Apellido, Password, Telefono, Correo, Saldo, Estado, FechaNacimiento]);
-        res.send('Usuario creado');
-        EnvioCorreo(Correo, Nombre, Apellido).catch(console.error);
+        const sql = 'CALL UsuarioCreate(?, ?, ?, ?, ?, ?, ?, ?, @OutResultCode)';
+        const params = [Nombre, Apellido, Password, Telefono, Correo, Saldo, Estado, FechaNacimiento];
+
+        const [result] = await MySqlConnection.execute(sql, params);
+
+        // Obtener el resultado del procedimiento almacenado
+        const [rows] = await MySqlConnection.query('SELECT @OutResultCode AS result_code');
+
+        // Obtener el resultado del código de resultado
+        const resultCode = rows[0].result_code;
+        console.log("Resultado: ", resultCode);
+
+        // Manejar el resultado y enviar la respuesta correspondiente
+        switch (resultCode) {
+            case 1:
+                res.status(200).json({ alert: 'success', message: 'Usuario creado exitosamente' });
+                break;
+            case 508:
+                res.status(400).json({ alert: 'error', message: 'Ya existe un usuario registrado con ese correo' });
+                break;
+            default:
+                res.status(500).json({ alert: 'error', message: 'Error desconocido' });
+                break;
+        }
     } catch (err) {
-        res.status(500).send(err.message);
+        console.error('Error al crear el usuario:', err);
+        res.status(500).json({ alert: 'error', message: 'Error en el servidor' });
     }
 };
