5814c480280665931454676695925d8c245a1141
Pamreth29 <pamreth29@gmail.com>
2024-07-02 15:11:12 -0600

    prueba

--- src/routers/rtLogin/RouterLogin.js
+++ src/routers/rtLogin/RouterLogin.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import {validateLogin} from "../../controllers/ctrLogin/validateLogin.js";
+import {validate} from "../../controllers/ctrLogin/validateLogin.js";
 
 //ruta para iniciar sesion
-router.post('/validate', validateLogin);
+router.post('/validate', validate);
 
 export default router;
