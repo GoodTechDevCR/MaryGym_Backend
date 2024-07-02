9a1be3c59e99cedb674721835632605f83f2905b
Pamreth29 <pamreth29@gmail.com>
2024-07-01 21:11:03 -0600

    Update RouterLogin.js

--- src/routers/rtLogin/RouterLogin.js
+++ src/routers/rtLogin/RouterLogin.js
@@ -4,6 +4,6 @@ const router = express.Router();
 import {validateLogin} from "../../controllers/ctrLogin/validateLogin.js";
 
 //ruta para iniciar sesion
-router.post('/validate', validateLogin);
+router.post('/validateLogin', validateLogin);
 
 export default router;
