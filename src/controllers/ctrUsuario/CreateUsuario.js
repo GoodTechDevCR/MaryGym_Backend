b46e07c2136cf123aa7f721edf69ba324bef7d9e
Luis <luisgerardourbsalz@gmail.com>
2024-07-02 18:22:11 -0600

    Update CreateUsuario.js

--- src/controllers/ctrUsuario/CreateUsuario.js
+++ src/controllers/ctrUsuario/CreateUsuario.js
@@ -1,12 +1,12 @@
 import { MySqlConnection } from '../../database/DBConnection.js';
-import {EnvioCorreo} from "../../services/EmailSender.js"
+import { EnvioCorreo } from "../../services/EmailSender.js";
 
 // Controlador para Crear usuario
 export const createUsuario = async (req, res) => {
-    const { Nombre, Apellido, Password, Telefono, Correo, Saldo, Estado, FechaNacimiento } = req.body;
+    const { Nombre, Apellido, Password, Telefono, Correo, Estado, FechaNacimiento, UltimoPago } = req.body;
     const sql = 'CALL UsuarioCreate(?, ?, ?, ?, ?, ?, ?, ?)';
     try {
-        const [result] = await MySqlConnection.execute(sql, [Nombre, Apellido, Password, Telefono, Correo, Saldo, Estado, FechaNacimiento]);
+        const [result] = await MySqlConnection.execute(sql, [Nombre, Apellido, Password, Telefono, Correo, Estado, FechaNacimiento, UltimoPago]);
         res.send('Usuario creado');
         EnvioCorreo(Correo, Nombre, Apellido).catch(console.error);
     } catch (err) {
