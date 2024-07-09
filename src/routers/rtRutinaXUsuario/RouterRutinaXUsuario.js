087275cfbd094e1d770fee3b8920547f4655d110
samcab28 <samircabrera2528@gmail.com>
2024-07-09 15:56:30 -0600

    update rutina

--- src/routers/rtRutinaXUsuario/RouterRutinaXUsuario.js
+++ src/routers/rtRutinaXUsuario/RouterRutinaXUsuario.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import {createRutinaXUsuario} from "../../controllers/ctrRutinaXUsuario/createRutinaXUsuario.js";
+import {createRutinaXUsuario} from "../../controllers/ctrRutinaXUsuario/CreateRutinaXUsuario.js";
 
 //ruta para crear un pago
 router.post('/', createRutinaXUsuario);
