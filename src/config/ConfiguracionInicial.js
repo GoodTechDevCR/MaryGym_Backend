f1b6df0a9e52723e94174aa4e1c142b3626bf61f
Pamreth29 <pamreth29@gmail.com>
2024-07-10 13:51:51 -0600

    avance modificar pago

--- src/config/ConfiguracionInicial.js
+++ src/config/ConfiguracionInicial.js
@@ -11,20 +11,20 @@ export const DB_PORT = process.env.DB_PORT || 29670
 
 
 //conexiones de base de datos provicional
-/*
+
 export const DB_HOST = process.env.DB_HOST || 'viaduct.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'YzwSvANfaldnyVYiqDJMTlPuTSbUJpXn'
 export const DB_NAME = process.env.DB_NAME || 'railway'
 export const DB_PORT = process.env.DB_PORT || 22412
-*/
 
+/*
 //conexiones de base de datos provicional
 export const DB_HOST = process.env.DB_HOST || '25.7.30.30'
 export const DB_USER = process.env.DB_USER || 'marygym'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Luis1234'
 export const DB_NAME = process.env.DB_NAME || 'marygym'
 export const DB_PORT = process.env.DB_PORT || 3306
-
+*/
 
 //clave de hamachi: LuisJosi
--- src/routers/rtPago/RouterPago.js
+++ src/routers/rtPago/RouterPago.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 import {createPago} from "../../controllers/ctrPago/CreatePago.js";
 
-import {consultPago} from "../../controllers/ctrPago/ConsultPago.js";
+import {consultPago, consultPagoPorID} from "../../controllers/ctrPago/ConsultPago.js";
 
 import {deletePago} from "../../controllers/ctrPago/DeletePago.js";
 
 import {updatePago} from "../../controllers/ctrPago/UpdatePago.js";
+
 
 //ruta para crear un pago
 router.post('/', createPago);
@@ -17,6 +18,9 @@ router.delete('/:id', deletePago);
 
 // Router para Consultar pagos o los pagos de un usuario (idUsuario)
 router.get('/:id?', consultPago);
+
+// Router para Consultar pago por medio de un ID
+router.get('/pagoId/:idPago', consultPagoPorID);
 
 //router para hacer un update de apgo
 router.put('/', updatePago);
