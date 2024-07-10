cbfc69544bc9a6cf296dbb3769e967874b3b5d33
Pamreth29 <pamreth29@gmail.com>
2024-07-10 12:35:23 -0600

    se agrega lo de rutina x usuario

--- src/routers/rtRutinaXUsuario/RouterRutinaXUsuario.js
+++ src/routers/rtRutinaXUsuario/RouterRutinaXUsuario.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import {createRutinaXUsuario} from "../../controllers/ctrRutinaXUsuario/createRutinaXUsuario.js";
+import {createRutinaXUsuario} from "../../controllers/ctrRutinaXUsuario/CreateRutinaXUsuario.js";
+import {consultRutinaXUsuario} from "../../controllers/ctrRutinaXUsuario/ConsultRutinaXUsuario.js";
 
 //ruta para crear un pago
 router.post('/', createRutinaXUsuario);
 
-//router.get('/', createRutinaXUsuario);
+router.get('/', consultRutinaXUsuario);
 
 export default router;
