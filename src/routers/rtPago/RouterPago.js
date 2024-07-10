cdea24dae7475539165fd08f92b0c159f9fd1c05
samcab28 <samircabrera2528@gmail.com>
2024-07-10 16:22:46 -0600

    Update RouterPago.js

--- src/routers/rtPago/RouterPago.js
+++ src/routers/rtPago/RouterPago.js
@@ -2,27 +2,23 @@ import express from 'express';
 const router = express.Router();
 
 import {createPago} from "../../controllers/ctrPago/CreatePago.js";
-
 import {consultPago, consultPagoPorID} from "../../controllers/ctrPago/ConsultPago.js";
-
 import {deletePago} from "../../controllers/ctrPago/DeletePago.js";
-
 import {updatePago} from "../../controllers/ctrPago/UpdatePago.js";
 
+// Router para Consultar pago por medio de un ID
+router.get('/pagoprueba/:idPago', consultPagoPorID);
 
-//ruta para crear un pago
+// Router para Consultar pagos o los pagos de un usuario (idUsuario)
+router.get('/:id?', consultPago);
+
+// Ruta para crear un pago
 router.post('/', createPago);
 
 // Router para Eliminar pago
 router.delete('/:id', deletePago);
 
-// Router para Consultar pagos o los pagos de un usuario (idUsuario)
-router.get('/:id?', consultPago);
-
-// Router para Consultar pago por medio de un ID
-router.get('/pagoId/:idPago', consultPagoPorID);
-
-//router para hacer un update de apgo
+// Router para hacer un update de pago
 router.put('/', updatePago);
 
 export default router;
