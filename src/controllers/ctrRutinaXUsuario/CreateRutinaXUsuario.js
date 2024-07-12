6c184c848e5b75e78ca15b01fb60ababffbb4b6d
samcab28 <samircabrera2528@gmail.com>
2024-07-12 15:02:55 -0600

    Update CreateRutinaXUsuario.js

--- src/controllers/ctrRutinaXUsuario/CreateRutinaXUsuario.js
+++ src/controllers/ctrRutinaXUsuario/CreateRutinaXUsuario.js
@@ -2,10 +2,14 @@ import { MySqlConnection } from '../../database/DBConnection.js';
 
 // Controlador para consultar un servicio
 export const createRutinaXUsuario = async (req, res) => {
-    const { idUsuario, Json } = req.body;
+    const { IdUsuario, Json } = req.body;
     const sql = 'CALL InsertarRutinaXUsuario(?, ?)';
+    
+    // Stringify the JSON object
+    const jsonString = JSON.stringify(Json);
+    
     try {
-        const [result] = await MySqlConnection.execute(sql, [idUsuario, Json]);
+        const [result] = await MySqlConnection.execute(sql, [IdUsuario, jsonString]);
         res.send(result[0]);
     } catch (err) {
         res.status(500).send(err.message);
